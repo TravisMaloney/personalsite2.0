c541f7fca209275de9e0cc4f0c4153de490c60cf
TravisMaloney <travis.maloney1@gmail.com>
2017-06-21 12:44:47 -0400

    PLEASE FIX SERVEr

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ app.engine('html', engines.mustache);
 app.set('view engine', 'html');
 
 // NEW CODE
-app.use(express.static(__dirname + 'public'));
-app.use(express.static(__dirname + 'vendor'));
-app.use(express.static(__dirname + 'img'));
-app.use(express.static(__dirname + 'css'));
-app.use(express.static(__dirname + 'js'));
+app.use('/public', express.static(__dirname + '/public'));
+app.use('/vendor', express.static(__dirname + '/vendor'));
+app.use('/img', express.static(__dirname + '/img'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
 
 
 
