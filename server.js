351bf8906d900cd0213dca82d00d810217a41ed9
TravisMaloney <travis.maloney1@gmail.com>
2017-06-21 10:52:40 -0400

    Please work

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ app.engine('html', engines.mustache);
 app.set('view engine', 'html');
 
 // NEW CODE
-app.use('/vendor', express.static(__dirname + '/vendor'));
-app.use('/img', express.static(__dirname + '/img'));
-app.use('/css', express.static(__dirname + '/css'));
-app.use('/js', express.static(__dirname + '/js'));
+app.use('/vendor', express.static(__dirname + '../vendor'));
+app.use('/img', express.static(__dirname + '../img'));
+app.use('/css', express.static(__dirname + '../css'));
+app.use('/js', express.static(__dirname + '../js'));
 
 
 
