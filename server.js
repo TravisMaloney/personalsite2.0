cd4075a2a74b5000e0de4f556b3e4692b3df92b0
TravisMaloney <travis.maloney1@gmail.com>
2017-06-21 12:13:02 -0400

    fixing css

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ app.engine('html', engines.mustache);
 app.set('view engine', 'html');
 
 // NEW CODE
-app.use('../vendor', express.static(__dirname + '../vendor'));
-app.use('../img', express.static(__dirname + '../img'));
-app.use('../css', express.static(__dirname + '../css'));
-app.use('../js', express.static(__dirname + '../js'));
+app.use('/public', express.static(__dirname + '/public'));
+app.use('/vendor', express.static(__dirname + '/vendor'));
+app.use('/img', express.static(__dirname + '/img'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
 
 
 
