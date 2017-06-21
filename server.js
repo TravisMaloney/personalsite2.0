793b37f0908481b0532d3b8e2a760413a45bb167
TravisMaloney <travis.maloney1@gmail.com>
2017-06-21 12:23:02 -0400

    fixing things?

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set('view engine', 'html');
 
 // NEW CODE
 app.use('/public', express.static(__dirname + '/public'));
-app.use('/vendor', express.static(__dirname + '/vendor'));
+app.use('vendor', express.static(__dirname + '/vendor'));
 app.use('/img', express.static(__dirname + '/img'));
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
