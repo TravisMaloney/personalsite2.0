06473468e70ade2c005502892185311c1e9161f8
TravisMaloney <travis.maloney1@gmail.com>
2017-06-22 12:28:59 -0400

    Fixing serer

--- server.js
+++ server.js
@@ -18,10 +18,6 @@ app.get("/", function(req,res){
     res.render('index.html')
 });
 
- app.get(/^(.+)$/, function(req, res){ 
-     console.log('static file request : ' + req.params);
-     res.sendfile( __dirname + req.params[0]); 
- });
 
 
 app.listen(3000, () => {
